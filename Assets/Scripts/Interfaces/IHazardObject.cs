fa5e65f9e2bbc716c6e4fe0ab8e845809a5ca2a2
Ibitayo <ibitayola@gmail.com>
2023-12-19 08:40:37 +0000

    fix: set up

--- Assets/Scripts/Interfaces/IHazardObject.cs
+++ Assets/Scripts/Interfaces/IHazardObject.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHazardObject
+﻿public interface IHazardObject
 {
     public void ActivateHazard();
 }

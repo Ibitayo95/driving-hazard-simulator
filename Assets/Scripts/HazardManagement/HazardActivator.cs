466fd1c935242e4462b3c419ab0bfd996fa4b1b1
Ibitayo <ibitayola@gmail.com>
2023-12-19 09:35:07 +0000

    feat: hazard activator and manager scripts

--- Assets/Scripts/HazardManagement/HazardActivator.cs
+++ Assets/Scripts/HazardManagement/HazardActivator.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+	Single script that exists on the user car to activate hazards that are within the user's field of view
+*/
+
 public class HazardActivator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private HazardManager hazardManager;
+    private static readonly System.Random oddsGenerator = new();
+    private int hazardHuman;
+    private int hazardCar;
+    // set in the editor
+    public float minHazardOffsetTime = 0f;
+    public float maxHazardOffsetTime;
+
+
     void Start()
     {
-        
+        hazardManager = hazardManager.GetInstance();
+        hazardHuman = LayerMask.NameToLayer("Humans");
+        hazardCar = LayerMask.NameToLayer("HazardCar");
     }
 
-    // Update is called once per frame
-    void Update()
+    // could activate the hazard - depends on the chance of it occuring
+    private void OnTriggerEnter(Collider other)
     {
-        
+        int objectLayer = other.gameObject.layer;
+
+        if (objectLayer == hazardHuman || objectLayer == hazardCar) // make sure we've triggered a hazard
+        {
+            // get hazard object
+            IHazardObject hazard = other.GetComponent<IHazardObject>();
+            if (hazard == null)
+            {
+                Debug.LogError("No hazard component detected on GameObject");
+                return;
+            }
+            // ignore if its outside the range of the attached trigger zone (e.g. 4-7 seconds)
+            if (!(hazard.HazardOffsetTime >= minHazardOffsetTime && hazard.HazardOffsetTime < maxHazardOffsetTime))
+            {
+                Debug.Log($"Hazard skipped because outside of trigger zone range: {hazard.Name}");
+                return;
+            }
+
+            // use odds to maybe activate hazard
+            int hazardOccurance = hazard.ChanceOfOccuring; // should be between 0 - 100
+            int chance = oddsGenerator.Next(0, 100);
+
+            if (hazardOccurance > chance)
+            {
+                hazardManager.ActivateHazard(hazard);
+            }
+        }
+
     }
 }
--- Assets/Scripts/HazardManagement/HazardManager.cs
+++ Assets/Scripts/HazardManagement/HazardManager.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HazardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static HazardManager Instance { get; private set; }
+
+    // This dictionary stores the reaction time for each hazard. 
+    // If a hazard is not present in the dictionary, it means the user did not react to it.
+    private readonly Dictionary<string, float> hazardReactionTimes = new();
+
+    private void Awake()
     {
-        
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public HazardManager GetInstance()
     {
-        
+        return Instance;
+    }
+
+    public void ActivateHazard(IHazardObject hazard)
+    {
+        // Activate the hazard 
+        hazard.ActivateHazard();
+        StartCoroutine(StartReactionTimer(hazard));
+
+    }
+
+    public IEnumerator StartReactionTimer(IHazardObject hazard)
+    {
+        // get the hazard's offsetTime first and wait for that number of seconds. Then start the timer 
+        float offset = hazard.HazardOffsetTime;
+        if (offset > 0)
+        {
+            yield return new WaitForSeconds(offset);
+        }
+        float reactionTime = 0;
+
+        while (reactionTime < 5)
+        {
+            // Test with pressing space first - will change this to VR input later
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Log the reaction time and end the timer
+                hazardReactionTimes[hazard.Name] = reactionTime;
+                yield break;
+            }
+            // If the user does not react, increment the timer
+            reactionTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // If the timer reaches 5 seconds without the user reacting, log that the user did not react to the hazard
+        hazardReactionTimes[hazard.Name] = -1;
+    }
+
+
+    public float GetAccuracyScore()
+    {
+        float numHazards = hazardReactionTimes.Count;
+        float numCorrectlyIdentified = 0;
+
+        foreach (KeyValuePair<string, float> entry in hazardReactionTimes)
+        {
+            if (entry.Value != -1)
+            {
+                numCorrectlyIdentified++;
+            }
+        }
+
+        return (numCorrectlyIdentified / numHazards) * 100;
     }
 }
+

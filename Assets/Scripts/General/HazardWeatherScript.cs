be3ac612baa6c9e31898b434cd27361db47850ea
Ibitayo <Ibitayola@gmail.com>
2024-02-18 11:50:46 +0000

    feat: using weighted list to simulate more realistic distribution of weather patterns

--- Assets/Scripts/General/HazardWeatherScript.cs
+++ Assets/Scripts/General/HazardWeatherScript.cs
@@ -3,52 +3,62 @@ using DistantLands.Cozy.Data;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KaimiraGames;
+using System.Linq;
 
 public class HazardWeatherScript : MonoBehaviour
 {
     public WeatherProfile[] WeatherProfiles;
-    private WeatherType[] WeatherTypes = new WeatherType[] { WeatherType.CLEAR, WeatherType.MIXEDCLOUD, WeatherType.LIGHTRAIN, WeatherType.HEAVYRAIN, WeatherType.THUNDERSTORM, WeatherType.FOG, WeatherType.SNOW };
-    private List<WeatherType> weatherTypeProbabilities = new();
-    private Dictionary<WeatherType, WeatherProfile> _weatherMap = new();
+    private readonly WeatherType[] _weatherTypes = new WeatherType[] { WeatherType.CLEAR, WeatherType.MIXEDCLOUD, 
+                                                                       WeatherType.LIGHTRAIN, WeatherType.HEAVYRAIN, 
+                                                                       WeatherType.THUNDERSTORM, WeatherType.FOG, WeatherType.SNOW }; // the order of this array matters: see order of WeatherProfiles array
+    private WeightedList<WeatherType> _weatherProbabilityArray;
+    private readonly Dictionary<WeatherType, WeatherProfile> _weatherMap = new();
     private bool weatherSet = false;
     private bool fogCheck = false;
+    private CozyWeather _globalWeather;
+
     public WeatherType currentWeatherType;
+    public WeatherProfile currentWeatherProfile;
+
 
     private void Awake()
     {
-        // set up weathermapping
+        // populate the weather map
         for (int i = 0; i < WeatherProfiles.Length; i++)
         {
-            _weatherMap[WeatherTypes[i]] = WeatherProfiles[i]; 
+            _weatherMap[_weatherTypes[i]] = WeatherProfiles[i]; 
         }
 
-        // populate random weather picker array: number from enum represents chance of being selected out of 20
-        for (int i = 0; i < WeatherTypes.Length; i++)
+        List<WeightedListItem<WeatherType>> items = new()
         {
-            int count = 0;
-            while (count < ((int) WeatherTypes[i]))
-            {
-                weatherTypeProbabilities.Add(WeatherTypes[i]);
-                count++;
-            }
-        }
-       
+            new WeightedListItem<WeatherType>(WeatherType.CLEAR, 40), // this means clear weather has a 40% chance of occuring (assuming all weights sum to 100)
+            new WeightedListItem<WeatherType>(WeatherType.MIXEDCLOUD, 20),
+            new WeightedListItem<WeatherType>(WeatherType.LIGHTRAIN, 15),
+            new WeightedListItem<WeatherType>(WeatherType.HEAVYRAIN, 10),
+            new WeightedListItem<WeatherType>(WeatherType.THUNDERSTORM, 5),
+            new WeightedListItem<WeatherType>(WeatherType.FOG, 5),
+            new WeightedListItem<WeatherType>(WeatherType.SNOW, 5),
+        };
+
+        _weatherProbabilityArray = new(items);
+
+        
     }
 
     void Start()
     {
-        // set a random weather type
-        int random = Random.Range(0, weatherTypeProbabilities.Count);
-        var weather = GetComponent<CozyWeather>();
-        
-        WeatherType randomWeather = weatherTypeProbabilities[random];
-        
+        _globalWeather = GetComponent<CozyWeather>();
 
-        foreach (CozyEcosystem i in weather.ecosystems)
+        // set a random weather type
+        WeatherType randomWeather = _weatherProbabilityArray.Next();
+        
+        foreach (CozyEcosystem i in _globalWeather.ecosystems)
         {
             i.SetWeather(_weatherMap[randomWeather]);
             weatherSet  = true;
             currentWeatherType = randomWeather;
+            currentWeatherProfile = _weatherMap[randomWeather];
         }
 
 
@@ -57,6 +67,7 @@ public class HazardWeatherScript : MonoBehaviour
 
     void Update()
     {
+
         if (weatherSet && !fogCheck)
         {
             var fog = GameObject.FindWithTag("Fog");
@@ -72,18 +83,26 @@ public class HazardWeatherScript : MonoBehaviour
 
             fogCheck = true;
         }
+
+        // if global weather pattern changes then update the script to match
+        if (_globalWeather.currentWeather != currentWeatherProfile)
+        {
+            currentWeatherProfile = _globalWeather.currentWeather;
+            currentWeatherType = _weatherMap.FirstOrDefault(x => x.Value == currentWeatherProfile).Key;
+            fogCheck = false;
+        }
+
+    
     }
 }
 
 public enum WeatherType
 {
-    // each number represents the likelyhood of the weather type being chosen at the start
-    // note that one cannot assign the same number to different weathers or unexpected errors arise
-    CLEAR = 7,
-    MIXEDCLOUD = 6,
-    LIGHTRAIN = 5,
-    HEAVYRAIN = 4,
-    THUNDERSTORM = 1,
-    FOG = 2,
-    SNOW = 3
+    CLEAR,
+    MIXEDCLOUD,
+    LIGHTRAIN,
+    HEAVYRAIN,
+    THUNDERSTORM,
+    FOG,
+    SNOW
 }

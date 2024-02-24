072eff2dc1ed6392d9a1c998b509af74392d3c9f
Ibitayo <ibitayola@gmail.com>
2024-02-24 21:14:50 +0000

    feat: Traffic light manager set up

---
+++ Assets/Scripts/Traffic/TrafficLightManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Traffic
+{
+    public class TrafficLightManager : MonoBehaviour
+    {
+        public List<TrafficLight> trafficLights = new();
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

96d290da67244ef5b07d3421c214461a6c2e33ea
Ibitayo <Ibitayola@gmail.com>
2024-02-24 23:45:22 +0000

    feat: traffic light manager logic

--- Assets/Scripts/Traffic/TrafficLightManager.cs
+++ Assets/Scripts/Traffic/TrafficLightManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,34 @@ namespace Traffic
 {
     public class TrafficLightManager : MonoBehaviour
     {
+        // assign in the editor
         public List<TrafficLight> trafficLights = new();
 
         private void Start()
         {
-            throw new NotImplementedException();
+            foreach (TrafficLight light in trafficLights)
+            {
+                light.redLight = true;
+            }
+
+            StartCoroutine(TurnGreenThenRed(trafficLights));
+        }
+
+        private IEnumerator TurnGreenThenRed(List<TrafficLight> trafficLights)
+        {
+            while (true)
+            {
+                foreach (TrafficLight trafficLight in trafficLights)
+                {
+                    trafficLight.amberLight = true;
+                    yield return new WaitForSecondsRealtime(1);
+                    trafficLight.greenLight = true;
+                    yield return new WaitForSecondsRealtime(5);
+                    trafficLight.amberLight = true;
+                    yield return new WaitForSecondsRealtime(1);
+                    trafficLight.redLight = true;
+                }
+            }
         }
     }
 }
